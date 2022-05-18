0a9a24cc930989232233aa22796c95593759c4de
Алексей Воеводин <voevodinal173@gmail.com>
2022-05-18 08:33:07 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('YATUBE - Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f' YARUBE - Группа {slug}')
